c79a619eeb345439bef92d60571c92cb173f68e7
dtbuild <dtbuild@rigil>
2026-07-22 13:40:40 +0000

    9357823b84064d8c7144c51fcce9080bec43ef0c Release 2.0.0
    Sync to source repo @9357823b84064d8c7144c51fcce9080bec43ef0c

--- js/columnControl.dataTables.min.mjs
+++ js/columnControl.dataTables.min.mjs
@@ -1,4 +1,4 @@
-/*! ColumnControl default styling 2.0.0-beta.1 for DataTables
+/*! ColumnControl default styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-dt";import ColumnControl from"datatables.net-columncontrol";export default DataTable;
